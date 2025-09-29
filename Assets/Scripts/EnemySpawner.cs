e82c952b267b9f8b32d01aace2e54a36423c7e99
XlightEZ <xlightez2003@gmail.com>
2025-09-29 11:52:44 +0200

    Spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+using NaughtyAttributes;
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Enemy Spawner Settings")]
+    [BoxGroup("Spawner Settings")]
+    public Transform[] spawnerPoints;
+    [BoxGroup("Spawner Settings")]
+    public GameObject enemyPrefab;
+    [BoxGroup("Spawner Settings")]
+    public float spawnInterval = 2f;
+    [BoxGroup("Spawner Settings")]
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            SpawnEnemy();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int index = Random.Range(0, spawnerPoints.Length);
+        Transform spawnPoint = spawnerPoints[index];
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
